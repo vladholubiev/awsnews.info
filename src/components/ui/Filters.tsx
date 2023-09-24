5f2584b7c7a33a4eb1d74d51a148dd628693ae09
Vlad Holubiev <golubev.vld@gmail.com>
2023-09-24 22:36:00 +0200

    feat: add scroll area

--- src/components/ui/Filters.tsx
+++ src/components/ui/Filters.tsx
@@ -3,6 +3,7 @@ import {useInstantSearch, useRefinementList, UseRefinementListProps} from 'react
 import {Checkbox} from "@/components/ui/checkbox";
 import {Label} from "@/components/ui/label"
 import {Switch} from "@/components/ui/switch"
+import {ScrollArea} from "@/components/ui/scroll-area"
 
 type CustomUseRefinementListProps = UseRefinementListProps & {
     facets: Record<string, number>;
@@ -70,29 +71,32 @@ export default function CustomRefinementList(props: CustomUseRefinementListProps
                 <Label htmlFor="toggle-all">Filter out all noise</Label>
             </div>
 
-            {facets.map((item) => {
-                return (
-                    <div className="flex items-center space-x-2 mb-4" key={item}>
-                        <Checkbox id={item} checked={refinedTags.some(t => t === item)}
-                                  disabled={results?.query !== ''}
-                                  onCheckedChange={(checked) => {
-                                      refine(`-${item}`);
+            <ScrollArea className="h-[400px] rounded-md border p-4">
 
-                                      if (checked) {
-                                          setRefinedTags([...refinedTags, item]);
-                                      } else {
-                                          setRefinedTags(refinedTags.filter(t => t !== item));
-                                      }
-                                  }}/>
-                        <label
-                            htmlFor={item}
-                            className="text-sm font-normal leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer"
-                        >
-                            {item} ({(props.facets as any)[item] ?? 0})
-                        </label>
-                    </div>
-                );
-            })}
+                {facets.map((item) => {
+                    return (
+                        <div className="flex items-center space-x-2 mb-4" key={item}>
+                            <Checkbox id={item} checked={refinedTags.some(t => t === item)}
+                                      disabled={results?.query !== ''}
+                                      onCheckedChange={(checked) => {
+                                          refine(`-${item}`);
+
+                                          if (checked) {
+                                              setRefinedTags([...refinedTags, item]);
+                                          } else {
+                                              setRefinedTags(refinedTags.filter(t => t !== item));
+                                          }
+                                      }}/>
+                            <label
+                                htmlFor={item}
+                                className="text-sm font-normal leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer"
+                            >
+                                {item} ({(props.facets as any)[item] ?? 0})
+                            </label>
+                        </div>
+                    );
+                })}
+            </ScrollArea>
             <div className="relative mt-8 mb-6">
                 <div className="absolute inset-0 flex items-center">
                     <span className="w-full border-t"/>
@@ -111,7 +115,7 @@ export default function CustomRefinementList(props: CustomUseRefinementListProps
             <br/>
             <p className="prose">
                 This is a better place to read AWS product announcements.
-                Especially if you are sick of posts like “aws xxx is available in region yyy”.
+                Especially if you are sick of posts like “<em>aws xxx is available in region yyy</em>”.
             </p>
         </div>
     );
