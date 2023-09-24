bab207115c89ec0e1ca29cde730fed4f577ff829
Vlad Holubiev <golubev.vld@gmail.com>
2023-09-24 23:53:19 +0200

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full bg-gray-50">
+        <head>
+          <link rel="icon" href="/favicon.png" />
+        </head>
       <body className={inter.className + ' h-full'}>{children}</body>
     </html>
   )
