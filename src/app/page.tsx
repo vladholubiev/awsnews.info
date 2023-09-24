387600dda60e53c0b8a4126d53bddcb8265e2694
Vlad Holubiev <golubev.vld@gmail.com>
2023-09-25 00:49:06 +0200

    fix: visible selected page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -186,7 +186,7 @@ export default function Home() {
                             }}/>
                             <Pagination classNames={{
                                 item: 'h-9 w-9 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
-                                selectedItem: 'bg-primary/90 text-white shadow hover:bg-primary/80 hover:text-primary-foreground',
+                                selectedItem: 'hover:bg-primary/80 hover:text-primary-foreground border-primary',
                                 disabledItem: 'hidden',
                                 link: 'flex items-center justify-center w-full h-full'
                             }}/>
