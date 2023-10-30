f99e5cfd3043cf8e54559fb1457370309752faed
Vlad Holubiev <golubev.vld@gmail.com>
2023-10-30 12:58:15 +0100

    chore: upd deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
-import React, {Fragment, useState} from 'react'
-import {Dialog, Transition} from '@headlessui/react'
-import {Bars3Icon, XMarkIcon,} from '@heroicons/react/24/outline'
+import React, { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon, } from '@heroicons/react/24/outline'
 import algoliasearch from 'algoliasearch/lite';
-import {Pagination, PoweredBy, SearchBox, Stats} from 'react-instantsearch';
-import {InstantSearchNext} from 'react-instantsearch-nextjs';
+import { Pagination, PoweredBy, SearchBox, Stats } from 'react-instantsearch';
+import { InstantSearchNext } from 'react-instantsearch-nextjs';
 import CustomRefinementList from "@/components/ui/Filters";
 import CustomHits from "@/components/ui/Hits";
 import Script from 'next/script'
@@ -20,7 +20,7 @@ export default function Home() {
 
     return (
         <>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BX0WFNT2Y0"/>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BX0WFNT2Y0" />
             <Script id="google-analytics">
                 {`
                     window.dataLayer = window.dataLayer || [];
@@ -30,7 +30,7 @@ export default function Home() {
                     gtag('config', 'G-BX0WFNT2Y0');
               `}
             </Script>
-            <InstantSearchNext searchClient={searchClient} indexName="awsnews">
+            <InstantSearchNext searchClient={searchClient} indexName="awsnews" future={{ preserveSharedStateOnUnmount: true }}>
                 <div>
                     <Transition.Root show={sidebarOpen} as={Fragment}>
                         <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
@@ -43,7 +43,7 @@ export default function Home() {
                                 leaveFrom="opacity-100"
                                 leaveTo="opacity-0"
                             >
-                                <div className="fixed inset-0 bg-gray-900/80"/>
+                                <div className="fixed inset-0 bg-gray-900/80" />
                             </Transition.Child>
 
                             <div className="fixed inset-0 flex">
@@ -68,20 +68,20 @@ export default function Home() {
                                         >
                                             <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
                                                 <button type="button" className="-m-2.5 p-2.5"
-                                                        onClick={() => setSidebarOpen(false)}>
+                                                    onClick={() => setSidebarOpen(false)}>
                                                     <span className="sr-only">Close sidebar</span>
-                                                    <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true"/>
+                                                    <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
                                                 </button>
                                             </div>
                                         </Transition.Child>
                                         <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
                                             <div className="relative mt-8">
                                                 <div className="absolute inset-0 flex items-center">
-                                                    <span className="w-full border-t"/>
+                                                    <span className="w-full border-t" />
                                                 </div>
                                                 <div className="relative flex justify-center text-xs uppercase">
                                                     <span className="bg-background px-2 text-muted-foreground">
-                                                      About
+                                                        About
                                                     </span>
                                                 </div>
                                             </div>
@@ -96,7 +96,7 @@ export default function Home() {
                                                 <a href="https://github.com/vladholubiev/awsnews.info" target="_blank">Open
                                                     Source!&nbsp;</a>
                                                 Especially if you are sick of posts like “<em>aws xxx is available in
-                                                region yyy</em>”.
+                                                    region yyy</em>”.
                                             </p>
                                         </div>
                                     </Dialog.Panel>
@@ -111,16 +111,16 @@ export default function Home() {
                             className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
                             <div className="hidden lg:block">
                                 <CustomRefinementList attribute="tags" operator="and" escapeFacetValues={false}
-                                                      facets={facets} limit={100}/>
+                                    facets={facets} limit={100} />
                             </div>
                             <div className="relative mt-8">
                                 <div className="absolute inset-0 flex items-center">
-                                    <span className="w-full border-t"/>
+                                    <span className="w-full border-t" />
                                 </div>
                                 <div className="relative flex justify-center text-xs uppercase">
-                                                    <span className="bg-background px-2 text-muted-foreground">
-                                                      About
-                                                    </span>
+                                    <span className="bg-background px-2 text-muted-foreground">
+                                        About
+                                    </span>
                                 </div>
                             </div>
                             <p className="prose">
@@ -134,7 +134,7 @@ export default function Home() {
                                 <a href="https://github.com/vladholubiev/awsnews.info" target="_blank">Open
                                     Source!&nbsp;</a>
                                 Especially if you are sick of posts like “<em>aws xxx is available in
-                                region yyy</em>”.
+                                    region yyy</em>”.
                             </p>
                         </div>
                     </div>
@@ -142,9 +142,9 @@ export default function Home() {
                     <div
                         className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
                         <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-                                onClick={() => setSidebarOpen(true)}>
+                            onClick={() => setSidebarOpen(true)}>
                             <span className="sr-only">Open sidebar</span>
-                            <Bars3Icon className="h-6 w-6" aria-hidden="true"/>
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                         </button>
                         <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
                             About
@@ -153,7 +153,7 @@ export default function Home() {
                             root: 'flex-none h-12 flex space-around items-center sm:hidden',
                             logo: 'h-4',
                             link: ''
-                        }}/>
+                        }} />
                     </div>
 
                     <main className="py-10 lg:pl-80">
@@ -164,32 +164,32 @@ export default function Home() {
                                     submitIcon: "hidden",
                                     resetIcon: "hidden",
                                     root: 'flex-1 h-12'
-                                }} placeholder="Start searching here"/>
+                                }} placeholder="Start searching here" />
                                 <PoweredBy classNames={{
                                     root: 'flex-none h-12 flex space-around items-center hidden sm:flex',
                                     logo: 'h-4',
                                     link: ''
-                                }}/>
+                                }} />
                             </div>
                             <div className="block lg:hidden">
                                 <CustomRefinementList attribute="tags" operator="and" escapeFacetValues={false}
-                                                      facets={facets} limit={100}/>
+                                    facets={facets} limit={100} />
                             </div>
 
                             <Stats classNames={{
                                 root: 'text-sm text-muted-foreground pb-6 pt-2 px-2'
-                            }}/>
+                            }} />
 
 
                             <CustomHits onFacetsUpdate={(newFacets) => {
                                 setFacets(newFacets);
-                            }}/>
+                            }} />
                             <Pagination classNames={{
                                 item: 'h-9 w-9 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
                                 selectedItem: 'hover:bg-primary/80 hover:text-primary-foreground border-primary',
                                 disabledItem: 'hidden',
                                 link: 'flex items-center justify-center w-full h-full'
-                            }}/>
+                            }} />
 
                         </div>
                     </main>
